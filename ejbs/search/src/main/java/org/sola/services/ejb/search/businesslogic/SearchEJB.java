e4e6bdfff0845671946359636af623ef846a9eb1
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2014-01-21 14:04:39 +0100

    #390 - Dashboard Application List Omission

--- ejbs/search/src/main/java/org/sola/services/ejb/search/businesslogic/SearchEJB.java
+++ ejbs/search/src/main/java/org/sola/services/ejb/search/businesslogic/SearchEJB.java
@@ -410,7 +410,7 @@ public class SearchEJB extends AbstractEJB implements SearchEJBLocal {
         params.put(CommonSqlProvider.PARAM_LANGUAGE_CODE, locale);
         params.put(CommonSqlProvider.PARAM_WHERE_PART, ApplicationSearchResult.QUERY_WHERE_GET_UNASSIGNED);
         params.put(CommonSqlProvider.PARAM_ORDER_BY_PART, ApplicationSearchResult.QUERY_ORDER_BY);
-        params.put(CommonSqlProvider.PARAM_LIMIT_PART, "100");
+//        params.put(CommonSqlProvider.PARAM_LIMIT_PART, "100");
 
         return getRepository().getEntityList(ApplicationSearchResult.class, params);
     }
@@ -446,7 +446,7 @@ public class SearchEJB extends AbstractEJB implements SearchEJBLocal {
         }
 
         params.put(CommonSqlProvider.PARAM_ORDER_BY_PART, ApplicationSearchResult.QUERY_ORDER_BY);
-        params.put(CommonSqlProvider.PARAM_LIMIT_PART, "100");
+//        params.put(CommonSqlProvider.PARAM_LIMIT_PART, "100");
 
         return getRepository().getEntityList(ApplicationSearchResult.class, params);
     }
