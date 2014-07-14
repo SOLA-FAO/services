47e1d32ecdc86dbe94a1c28fa8251889bb3e8564
Elton Manoku <emanoku@gmail.com>
2014-07-14 17:06:43 +0200

    Change grid display and bug in Title plan generation

--- ejbs/search/src/main/java/org/sola/services/ejb/search/businesslogic/SearchEJB.java
+++ ejbs/search/src/main/java/org/sola/services/ejb/search/businesslogic/SearchEJB.java
@@ -564,7 +564,6 @@ public class SearchEJB extends AbstractEJB implements SearchEJBLocal {
     public List<ConfigMapLayer> getConfigMapLayerList(String languageCode) {
         Map params = new HashMap<String, Object>();
         params.put(CommonSqlProvider.PARAM_LANGUAGE_CODE, languageCode);
-        params.put(CommonSqlProvider.PARAM_WHERE_PART, ConfigMapLayer.QUERY_WHERE_ACTIVE);
         params.put(CommonSqlProvider.PARAM_ORDER_BY_PART, ConfigMapLayer.QUERY_ORDER_BY);
         return getRepository().getEntityList(ConfigMapLayer.class, params);
     }
